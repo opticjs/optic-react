953746271e3ed9f2ec7400cef56f2b901687dc41
Alex Lopatin <alex@siftscience.com>
2015-08-03 04:03:55 -0700

    createContainer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,50 @@ var Optic = require('optic'),
     React = require('react');
 
 export function createContainer(ComponentClass, options) {
+  console.log('options: ');
+  console.log(options);
+
   return class OpticContainer extends React.Component {
+    componentWillMount() {
+      this._params = options.initialParams && options.initialParams() || {};
+      this._submitQueries();
+    },
+
+    _submitQueries() {
+      var queries = options.queries();
+      Object.keys(queries).forEach(name => {
+        var query = queries[name];
+
+        if (query instanceof Optic.Query) {
+          query.submit(response => {
+            this.setState({
+              [name]: response
+            });
+          }, provisionalResponse => {
+            this.setState({
+              [name]: provisionalResponse
+            });
+          });
+        } else {
+          this.setState({
+            [name]: query
+          });
+        }
+      });
+    },
+
+    _newParams(newParams = {}) {
+      Object.assign(this._params, newParams);
+      this._submitQueries();
+    },
+
     render() {
-      console.log('passing through optic-react');
+      var queries = options.queries();
+      var callbacks = options.callbacks && options.callbacks(this._newParams) || {};
+      var props = Object.assign({}, this.props, this.state, callbacks);
+
       return (
-        <ComponentClass {...this.props}>
+        <ComponentClass {...props}>
           {this.props.children}
         </ComponentClass>
       );
